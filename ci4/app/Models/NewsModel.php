2172fc275459d7d2131d2f93e10a4f7b358cda31
anmendoza24 <anmendoza@student.apc.edu.ph>
2023-03-01 12:56:22 +0800

    Update NewsModel.php

--- ci4/app/Models/NewsModel.php
+++ ci4/app/Models/NewsModel.php
@@ -4,8 +4,13 @@ namespace App\Models;
 use CodeIgniter\Model;
 class NewsModel extends Model 
 {
+<<<<<<< HEAD
     protected $table = 'news';  
     protected $allowedFields = ['title', 'slug', 'body'];
+=======
+    protected $table = 'anmendoza_news';  
+
+>>>>>>> 79f5165b03f5f2441d88fe7f1a91d38d60e27ae3
     public function getNews ($slug = false)
     {
         if ($slug === false) {
@@ -13,5 +18,9 @@ class NewsModel extends Model
         }
         return $this->where(['slug' =>  $slug]) -> first();
     }
+<<<<<<< HEAD
 
 }
+=======
+}
+>>>>>>> 79f5165b03f5f2441d88fe7f1a91d38d60e27ae3
