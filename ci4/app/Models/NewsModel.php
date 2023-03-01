b9ceb9ca8209e03fad050ebc3031498563982214
anmendoza24 <119904163+anmendoza24@users.noreply.github.com>
2023-03-01 13:04:38 +0800

    Update NewsModel.php

--- ci4/app/Models/NewsModel.php
+++ ci4/app/Models/NewsModel.php
@@ -4,7 +4,7 @@ namespace App\Models;
 use CodeIgniter\Model;
 class NewsModel extends Model 
 {
-    protected $table = 'news';  
+    protected $table = 'anmendoza_news';  
     
     protected $allowedFields = ['title', 'slug', 'body'];
 
